3ff0f66b502f0c77649b924599e7d70e5b53f992
Tristan Elliott <tristan@mintsmm.com>
2019-10-31 08:59:38 +0200

    Styles Added

--- functions.php
+++ functions.php
@@ -120,9 +120,9 @@ add_action( 'widgets_init', 'bspremium_widgets_init' );
  * Enqueue scripts and styles.
  */
 function bspremium_scripts() {
-	wp_enqueue_style( 'bspremium-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bspremium-style', get_template_directory_uri() . '/assets/dist/css/bspremium.css' );
 
-	wp_enqueue_script( 'bspremium-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'bspremium-navigation', get_template_directory_uri() . '/assets/dist/js/bootstrap.min.js', array( 'jquery' ), '4.3.1', true );
 
 	wp_enqueue_script( 'bspremium-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
